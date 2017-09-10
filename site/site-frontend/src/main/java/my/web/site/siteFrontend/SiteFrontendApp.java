d78b7ab2864a292c849367c1030ae467bcdb3782
TKLatWork <sky71767@gmail.com>
2017-09-10 17:00:52 +0800

    init commit

---
+++ site/site-frontend/src/main/java/my/web/site/siteFrontend/SiteFrontendApp.java
@@ -0,0 +1,13 @@
+package my.web.site.siteFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteFrontendApp {
+
+    public static void main(String... args){
+        SpringApplication.run(SiteFrontendApp.class);
+    }
+
+}
